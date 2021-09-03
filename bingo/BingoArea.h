ae235d34fb1954c86a56642ba0d9ef4f60fc5bb3
tomo0716hista <nkhs.hc39.1060@gmail.com>
2021-09-03 16:15:46 +0900

    エラー修正

--- bingo/BingoArea.h
+++ bingo/BingoArea.h
@@ -19,14 +19,14 @@ public:
     void initStorage();    //ブロック置き場の初期化
     gameCoordinates objCoordinates(object object_type, int objrct_num);    //コース内要素座標を問い合わせる
     runInfo runningInformation();    //走行体情報を問い合わせる
-    Color circleColor(gameCoordinates pass_coordinates);    //交点サークルの色を問い合わせる
+    color circleColor(gameCoordinates pass_coordinates);    //交点サークルの色を問い合わせる
     int blockColor(int block_num);    //ブロックの色を問い合わせる
     bool objSuccsesPass(object object_type, int object_num);    //コース内要素が有効移動成立しているか問い合わせる
-    gameCoordinates relativeCoordinates(gameCoordinates coordinates, Direction rb_direction);    //相対座標系に変換する
-    gameCoordinates absoluteCoordinates(gameCoordinates coordinates, Direction rb_direction);    //絶対座標系に変換する
+    gameCoordinates relativeCoordinates(gameCoordinates coordinates, direction rb_direction);    //相対座標系に変換する
+    gameCoordinates absoluteCoordinates(gameCoordinates coordinates, direction rb_direction);    //絶対座標系に変換する
     bool coordinatesConparison(gameCoordinates pass_coordinates, gameCoordinates g_coordinates);    //座標比較同値ならtrue
-    void updateRunInfo(gameCoordiantes new_coodiantes, Direction new_direction, Direction old_direction);    //走行体情報を更新する
-    Direction convertDirection(Direction run_direction, Direction relative_direction);    //相対座標系における方向を絶対座標系に変換する
+    void updateRunInfo(gameCoordinates new_coordinates, direction new_direction, direction old_direction);    //走行体情報を更新する
+    direction convertDirection(direction run_direction, direction relative_direction);    //相対座標系における方向を絶対座標系に変換する
     
 private:
     RunningBody* run_body;
