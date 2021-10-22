4e2ca603e83fe8c028a0bc63c4c4b5d5ab779c6e
tomo0716hista <nkhs.hc39.1060@gmail.com>
2021-10-22 10:26:07 +0900

    最初のe_coodは正しくなったえけど無限ループ

--- bingo/BlockDetermination.cpp
+++ bingo/BlockDetermination.cpp
@@ -11,21 +11,24 @@ int BlockDetermination::routeDecision()    //ルートを決定する
 	num = 0;
 	min_cost = 999;
 	double cost;
+	gameCoordinates goal;
     run_info = bingo_area->runningInformation();    //走行体情報を問い合わせる
 	for (int i = 1; i < BLOCK_COUNT; i++)
 	{
         if (!bingo_area->objSuccsesPass(BLOCK, i))/*有効移動が成立しているか*/
         {
-	        goal_coordinates = bingo_area->objCoordinates(BLOCK, i);    //ブロック座標を問い合わせる
-	        cost = block_list->getCost(run_info.rb_coordinates, goal_coordinates);    //コストを取得する
+	        goal = bingo_area->objCoordinates(BLOCK, i);    //ブロック座標を問い合わせる
+	        cost = block_list->getCost(run_info.rb_coordinates, goal);    //コストを取得する
 	        if (minCompare(min_cost, cost))    //更新処理
             {
 				//goal_coordinates本決まりさせなきゃいけないよ
+				goal_coordinates = goal;
 		        min_cost = cost;
 		        num = i;
 	        }
         }
 	}
+	printf("goal.x = %lf, y = %lf\n", goal_coordinates.x, goal_coordinates.y);
 	bingo_area->updateTransportStatus(BLOCK, num);
     return num;    //取得したブロックナンバーをリターンする
 }
