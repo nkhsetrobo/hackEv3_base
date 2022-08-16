0c748247db89a2924a1b42d2510ae72f63553503
Masayuki HIKICHI <m.hikichi.1969@gmail.com>
2022-08-16 11:42:20 +0100

    HackEv3.h を移動して再追加

---
+++ HackEv3.h
@@ -0,0 +1,25 @@
+#ifndef __HACKEV3_H__
+#define __HACKEV3_H__
+
+#if defined(MAKE_SIM)
+const float D_RIGHT=9.00;
+const float D_LEFT=9.0;
+const float TREAD=13.8;
+#else
+const float D_RIGHT=10.00;
+const float D_LEFT=10.0;
+const float TREAD=12.83;
+#endif
+//14.3
+
+#define GREEN_H 118
+#define RED_H 0
+#define YELLOW_H 62
+#define BLUE_H 225
+
+#if defined(MAKE_RASPIKE)
+//#define PRIMARY
+//#define MAKE_RIGHT
+#endif
+
+#endif
